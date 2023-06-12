c314cf4c2bf4ceb36b17bc911bda5d5aa4730d53
Alaa Alassi <alaa.alassi@tno.nl>
2023-06-12 11:26:20 +0200

    bt trees hello world

---
+++ simpletree.py
@@ -0,0 +1,50 @@
+import py_trees as pt
+
+# Define the actions (leaf nodes)
+class ActionA(pt.behaviour.Behaviour):
+    def __init__(self, name):
+        super(ActionA, self).__init__(name)
+
+    def update(self):
+        # Perform action A
+        print("Performing Action A")
+        return pt.common.Status.SUCCESS
+
+
+class ActionB(pt.behaviour.Behaviour):
+    def __init__(self, name):
+        super(ActionB, self).__init__(name)
+
+    def update(self):
+        # Perform action B
+        print("Performing Action B")
+        return pt.common.Status.SUCCESS
+
+
+class ActionC(pt.behaviour.Behaviour):
+    def __init__(self, name):
+        super(ActionC, self).__init__(name)
+
+    def update(self):
+        # Perform action C
+        print("Performing Action C")
+        return pt.common.Status.SUCCESS
+
+
+# Define the behavior tree
+root = pt.composites.Sequence("Root", False)
+
+task_a = ActionA("Task A")
+task_b = ActionB("Task B")
+task_c = ActionC("Task C")
+
+root.add_child(task_a)
+root.add_child(task_b)
+root.add_child(task_c)
+
+# Create a behavior tree and tick it
+tree = pt.trees.BehaviourTree(root)
+
+# Run the behavior tree
+tree.setup(timeout=15)
+tree.tick_tock(500)
---
+++ testing-environment.Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.9
+
+WORKDIR /home
+
+RUN pip install numpy py-trees
+
